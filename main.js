b1cf38c0593a20c74124339aa161d826448d37e4
andrescorredorrincon <felipecorredor9010@gmail.com>
2021-04-09 20:50:33 -0500

    09-04-2021

---
+++ main.js
@@ -0,0 +1,43 @@
+
+
+// "Papas", "Yuca", "Cebolla", "Tomate", "Pepino"
+let lista_plaza2 = ["Papas", "Yuca", "Cebolla", "Tomate", "Pepino"];
+let lista_plaza = [];
+lista_plaza[0] = "Papas";
+lista_plaza[1] = "Yuca";
+lista_plaza[2] = "Cebolla";
+lista_plaza[3] = "Tomate";
+lista_plaza[4] = "Pepino";
+lista_plaza[5] = "Arroz";
+
+console.log(lista_plaza);
+for (let i = 0; i < lista_plaza.length; i++) {
+    console.log(lista_plaza[i]);
+}
+
+let array = [];
+
+for (let i = 0; i < 10; i++) {
+    array[i] = `Datos ${i}`;
+}
+console.log(array);
+
+
+
+let datos = [];
+
+datos.push("Miguel");
+datos.push("Angel");
+datos.push("Castro");
+let devuelta = datos.push("Escamilla");
+
+console.log(devuelta);
+console.log(datos);
+
+
+
+
+
+
+
+
